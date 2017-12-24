61deabb03715d7e0232c848a517b1ac44f571dbf
DivyanshPatel <nealbay@gmail.com>
2017-12-25 04:59:16 +0530

    Login Form and firebase auth setup

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React , {Component } from 'react';
 import {View, Text } from 'react-native';
+import firebase from 'firebase';
 import {Header} from './components/common';
+import LoginForm from './components/LoginForm';
+import config from './components/config';
 
 class App extends Component {
+  componentWillMount(){
+    firebase.initializeApp(config);
+  }
+
+
   render(){
     return (
       <View>
       <Header headerText='Authentication!!'/>
-      <Text>An App!!</Text>
+      <LoginForm />
     </View>
     );
   }
