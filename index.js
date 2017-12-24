4b7d0c5f5bc7535b83a29fb1a4295795d0969ba2
DivyanshPatel <nealbay@gmail.com>
2017-12-25 04:24:09 +0530

    Project init and added reusable component from albums

---
+++ index.js
@@ -0,0 +1,5 @@
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+import Header from './src/components/common'
+
+AppRegistry.registerComponent('auth', () => App);
---
+++ src/components/common/Button.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import {Text , TouchableOpacity} from 'react-native';
+
+const Button = (props) => {
+  return (
+    <TouchableOpacity onPress={props.onPress} style={styles.buttonStyle}>
+      <Text style = {styles.textStyle}>
+        {props.children}
+      </Text>
+    </TouchableOpacity>
+
+  );
+};
+
+const styles = {
+  textStyle:{
+    alignSelf: 'center',
+    fontSize: 16,
+    color: '#007aff',
+    fontWeight: '600',
+    paddingTop: 10,
+    paddingBottom: 10
+  },
+  buttonStyle: {
+    flex: 1,
+    alignSelf: 'stretch',
+    backgroundColor: '#fff',
+    borderRadius: 5,
+    borderColor: '#007aff',
+    borderWidth: 1,
+    marginLeft: 5,
+    marginRight: 5
+  }
+};
+
+export  {Button};
